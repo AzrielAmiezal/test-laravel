ade0e558020119be6b5cc24ec1de57055b6a88f7
Azriel Amiezal <67147873+AzrielAmiezal@users.noreply.github.com>
2023-05-05 03:52:32 +0800

    Add Searching & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,20 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+        // vardump search == dd(request('search'));
+
+        //title for All Post in 'category name'
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        //title for All Post by 'author name'
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
+
         return view(
             'posts',
             [
-                "title" => "All Posts",
+                "title" => "All Posts" . $title,
                 "active" => 'posts',
                 //"posts" => Post::all()
-                "posts" => Post::latest()->get()
+                //"posts" => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+                // using paginate() instead of get()
+                "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
             ]
         );
     }
