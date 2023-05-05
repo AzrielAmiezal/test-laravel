be011444413c487f38baf41feda7f54e05d37525
Azriel Amiezal <67147873+AzrielAmiezal@users.noreply.github.com>
2023-05-05 21:54:29 +0800

    Dashboard UI

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,14 +21,14 @@ use App\Http\Controllers\RegisterController;
 Route::get('/', function () {
     return view('home', [
         "title" => "Home",
-        "active" => 'home'
+
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "active" => "about",
+
         "name" => "Azriel Amiezal",
         "email" => "amiezalazwary@gmail.com",
         "image" => "resume.jpg"
@@ -42,21 +42,25 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
-        'active' => 'categories',
+
         'categories' => Category::all()
     ]);
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
+
 Route::post('/logout', [LoginController::class, 'logout']);
-
-
 
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 
 // Route::get('/categories/{category:slug}', function (Category $category) {
